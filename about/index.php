6ab00c49c114574e3336e04285c17274f58e392b
JonhyBack <djwar0962@gmail.com>
2021-11-25 21:47:51 +0200

    added google analytics

--- about/index.php
+++ about/index.php
@@ -4,6 +4,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Anime site</title>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-68H9K2W6BE"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-68H9K2W6BE');
+    </script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet"
