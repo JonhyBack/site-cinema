ba0113f612a238b80c1a90ac8e1df01b1070d22e
JonhyBack <djwar0962@gmail.com>
2021-11-25 12:10:11 +0200

    MySql config

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
 require "vendor/rb-mysql.php";
-R::setup('mysql:host=localhost;dbname=course_project', 'root', '');
+R::setup('mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_71e85780221d1d9', 'bdc416569553e8', '6ea177f6');
 session_start();
 ?>
