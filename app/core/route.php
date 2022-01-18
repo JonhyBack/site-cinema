94a6d601a5dbabc8ba4ab629de7ab026565f6f8d
JonhyBack <djwar0962@gmail.com>
2022-01-18 04:13:12 +0200

    htaccess changed 18

--- app/core/route.php
+++ app/core/route.php
@@ -36,9 +36,7 @@ class Route
         $controller_path = "app/controllers/" . $controller_file;
 
         try {
-
-                include_once $model_path;
-
+            include_once $model_path;
             require_once $controller_path;
 
             $controller = new $controller_name();
